03d7df812d09c567955363d87b05c698472b7f21
SEtester <dengjiajie0109@gmail.com>
2019-04-30 21:30:55 +0800

    增加打开浏览器的方式 chrome-headless

--- common/driver.py
+++ common/driver.py
@@ -1,12 +1,19 @@
 #encoding:utf-8
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 
 def start_up_driver(browser_name='Chrome'):
     if browser_name == 'Chrome':
         driver = webdriver.Chrome()
+    elif browser_name == "chrome_headless":
+        options = Options()
+        options.add_argument("--headless")  # Runs Chrome in headless mode.
+        options.add_argument('--no-sandbox')  # Bypass OS security model
+        options.add_argument('--disable-gpu')  # applicable to windows os only
+        driver = webdriver.Chrome(options=options)
     elif browser_name == 'Firefox':
         driver = webdriver.Firefox()
     elif browser_name == 'Ie':
@@ -17,5 +24,5 @@ def start_up_driver(browser_name='Chrome'):
         driver = webdriver.Safari()
     else:
         raise NameError(
-            "Not found %s browser,You can enter 'Chrome', 'Firefox', 'Ie', 'Edge', 'Safari' ." % browser_name)
+            "Not found %s browser,You can enter 'Chrome', 'Firefox', 'Ie', 'Edge', 'Safari','chrome-headless'." % browser_name)
     return driver
